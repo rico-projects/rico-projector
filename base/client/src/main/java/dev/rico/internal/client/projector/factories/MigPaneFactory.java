41059b65e9479f78f0e4374694f3c976c85f0e2a
Markus Schlichting <markus.schlichting@karakun.com>
2019-06-06 11:30:27 +0200

    implement MigPaneFactory

---
+++ base/client/src/main/java/dev/rico/internal/client/projector/factories/MigPaneFactory.java
@@ -0,0 +1,33 @@
+package dev.rico.internal.client.projector.factories;
+
+import dev.rico.client.projector.Projector;
+import dev.rico.client.projector.spi.ProjectorNodeFactory;
+import dev.rico.internal.projector.ui.IdentifiableModel;
+import javafx.scene.Node;
+import org.tbee.javafx.scene.layout.MigPane;
+import to.remove.ui.migpane.MigPaneModel;
+
+public class MigPaneFactory implements ProjectorNodeFactory<MigPaneModel, MigPane> {
+
+    @Override
+    public MigPane create(Projector projector, MigPaneModel model) {
+        MigPane migPane = new MigPane(model.getLayoutConstraints(), model.getColumnConstraints(), model.getRowConstraints());
+        model.getItems().forEach(migPaneItemModel -> {
+            Node child = projector.createNode(migPaneItemModel.getItem());
+            migPane.add(child, migPaneItemModel.getConstraints());
+        });
+        model.getItems().onChanged(evt -> {
+            migPane.getChildren().clear();
+            model.getItems().forEach(migPaneItemModel -> {
+                Node child = projector.createNode(migPaneItemModel.getItem());
+                migPane.add(child, migPaneItemModel.getConstraints());
+            });
+        });
+        return migPane;
+    }
+
+    @Override
+    public Class<MigPaneModel> getSupportedType() {
+        return MigPaneModel.class;
+    }
+}
