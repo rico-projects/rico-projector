300f6c87f6bda4344ed1b4328ce6b7eb99b2b08d
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-06-06 11:58:55 +0200

    more components

---
+++ base/client/src/main/java/dev/rico/internal/client/projector/factories/MessagePlaceholderFactory.java
@@ -0,0 +1,29 @@
+package dev.rico.internal.client.projector.factories;
+
+import dev.rico.client.projector.Projector;
+import dev.rico.client.projector.spi.ProjectorNodeFactory;
+import dev.rico.internal.projector.ui.PasswordFieldModel;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.paint.Color;
+import to.remove.ui.MessagePlaceholder;
+
+public class MessagePlaceholderFactory implements ProjectorNodeFactory<MessagePlaceholder, Label> {
+
+    @Override
+    public Label create(final Projector projector, final MessagePlaceholder model) {
+        final Label label = new Label();
+        label.setManaged(false);
+        label.setVisible(false);
+        label.setTextFill(Color.RED);
+        label.setPadding(new Insets(0, 0, 0, 10));
+        label.setWrapText(true);
+        return label;
+    }
+
+    @Override
+    public Class<MessagePlaceholder> getSupportedType() {
+        return MessagePlaceholder.class;
+    }
+}
