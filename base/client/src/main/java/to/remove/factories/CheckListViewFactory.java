a30a7972141bd57282caa2c3e77c56684034e600
Jörg Hälker <joerg.haelker@sprouts.aero>
2019-06-06 12:16:50 +0200

    Add CheckListViewFactory factory

---
+++ base/client/src/main/java/to/remove/factories/CheckListViewFactory.java
@@ -0,0 +1,23 @@
+package to.remove.factories;
+
+import dev.rico.client.projector.Projector;
+import dev.rico.client.projector.spi.ProjectorNodeFactory;
+import dev.rico.internal.core.Assert;
+import to.remove.ui.checklistview.CheckListViewModel;
+import to.remove.uimanager.CheckListView;
+
+public class CheckListViewFactory implements ProjectorNodeFactory<CheckListViewModel, CheckListView> {
+
+    @Override
+    public CheckListView create(final Projector projector, final CheckListViewModel model) {
+        Assert.requireNonNull(projector, "projector");
+        Assert.requireNonNull(model, "model");
+
+        return new CheckListView(model);
+    }
+
+    @Override
+    public Class<CheckListViewModel> getSupportedType() {
+        return CheckListViewModel.class;
+    }
+}
