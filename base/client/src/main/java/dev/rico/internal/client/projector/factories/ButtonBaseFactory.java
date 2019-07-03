de5b806bdae81a4e3aa5643fda2ec207f5df7607
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-07-03 13:02:27 +0200

    Merge branch 'master' into client-refactored-2
    # Conflicts:
    #	base/client/src/main/java/dev/rico/internal/client/projector/factories/ButtonBaseFactory.java

--- base/client/src/main/java/dev/rico/internal/client/projector/factories/ButtonBaseFactory.java
+++ base/client/src/main/java/dev/rico/internal/client/projector/factories/ButtonBaseFactory.java
@@ -7,16 +7,11 @@ import dev.rico.internal.core.Assert;
 import dev.rico.internal.projector.mixed.CommonUiHelper;
 import dev.rico.internal.projector.ui.ButtonModel;
 import dev.rico.remoting.Property;
-import javafx.beans.property.ObjectProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.function.Consumer;
 
 import static dev.rico.client.remoting.FXBinder.bind;
@@ -43,31 +38,6 @@ public abstract class ButtonBaseFactory<T extends ButtonModel, S extends ButtonB
         bind(node.textProperty()).to(model.captionProperty());
         subscribe(model.tooltipProperty(), tooltipOptional -> createTooltip(tooltipOptional, node));
         subscribe(model.imageProperty(), optionalImagePath -> createImage(optionalImagePath, model, node));
-    }
-
-    private void installMonitoredAction(final S node, final EventHandler<ActionEvent> onActionHandler) {
-        Assert.requireNonNull(node, "node");
-
-        final List<EventHandler<ActionEvent>> handlers = new LinkedList<>();
-        final EventHandler<ActionEvent> actionEventEventHandler = event -> handlers.forEach(handler -> handler.handle(event));
-        handlers.add(onActionHandler);
-        if (node.getOnAction() != null) {
-            handlers.add(node.getOnAction());
-        }
-        node.setOnAction(actionEventEventHandler);
-        final ObjectProperty<EventHandler<ActionEvent>> actionHandlerProperty = node.onActionProperty();
-        Assert.requireNonNull(actionHandlerProperty, "actionHandlerProperty");
-        actionHandlerProperty.addListener((observable, oldValue, newValue) -> {
-            if (newValue == actionEventEventHandler) {
-                return;
-            }
-            node.setOnAction(actionEventEventHandler);
-            handlers.clear();
-            handlers.add(onActionHandler);
-            if (newValue != null) {
-                handlers.add(newValue);
-            }
-        });
     }
 
     private void createTooltip(final String tooltipText, final S node) {
